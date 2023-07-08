c449ee7d81f21598ec842f2b993a352850c5b619
narayanprusty <arsenal.narayan@gmail.com>
2023-07-09 00:00:24 +0100

    updated withdraw

--- deploy/3-test.ts
+++ deploy/3-test.ts
@@ -7,17 +7,14 @@ module.exports = async ({getNamedAccounts, deployments} : HardhatRuntimeEnvironm
   const bridge:Bridge = await hre.ethers.getContract("Bridge");
   const epic:Token = await hre.ethers.getContract("EPIC");
 
-  console.log(epic) 
-  return;
- 
   let  tx = await bridge.processTransaction(
     "0xcD0B7D95DdeEe386A8de74A2515392e250aB4fFE",
-    17,
+    0,
     2,
     1000,
     epic.address,
     [
-      "0x36b6c02de668078fed4b6d2bb77a623c5ce1b5a65f8c6804a6ff8f3bc3b8b5f369c1820907b831f0aa5a99a0071420f38557d0def9120148b507a2e31b4333291c"
+      "0x0ce5ec70ab00dd97c3ab0c269960b576abbcc3640e9d0eb9946da973ad8ddcfa41b38895a81ca2c8d7f0730d0585de723970fcc2a0738a58030e12a658709b431b"
     ]
   );
   await tx.wait(1);
