1775d575352cd86f94981e570b56cf12fb7a4a67
narayanprusty <arsenal.narayan@gmail.com>
2023-08-30 16:23:28 +0100

    updated test script

--- deploy/3-test.ts
+++ deploy/3-test.ts
@@ -5,21 +5,42 @@ import { Token } from "typescript";
 
 module.exports = async ({getNamedAccounts, deployments} : HardhatRuntimeEnvironment) => {
   const bridge:Bridge = await hre.ethers.getContract("Bridge");
-  const epic:Token = await hre.ethers.getContract("EPIC");
+
+  const VALIDATOR_ONE_PRIVATE_KEY = ""
+  const VALIDATOR_TWO_PRIVATE_KEY = ""
+  const EPIC_ADDRESS = "0xF77B59a2Fd8eA0dED3eE72A2942C298De8A3e802";
+  const messageHash = await bridge.getMessageHash(
+    "0xEc788F2D9A68274dF239b9b58f2593a95ec139f5",
+    "100",
+    2,
+    "7000000000000000000000000",
+    EPIC_ADDRESS,
+  )
+
+  const validatorOne = new hre.ethers.Wallet(VALIDATOR_ONE_PRIVATE_KEY)
+  const validatorTwo = new hre.ethers.Wallet(VALIDATOR_TWO_PRIVATE_KEY)
+
+  const messageHashBinaryOne = hre.ethers.utils.arrayify(messageHash);
+  const signatureOne = await validatorOne.signMessage(messageHashBinaryOne);
+
+  const messageHashBinaryTwo = hre.ethers.utils.arrayify(messageHash);
+  const signatureTwo = await validatorTwo.signMessage(messageHashBinaryOne);
+
+  // console.log(await bridge.totalValidators())
 
   let  tx = await bridge.processTransaction(
-    "0xcD0B7D95DdeEe386A8de74A2515392e250aB4fFE",
-    0,
+    "0xEc788F2D9A68274dF239b9b58f2593a95ec139f5",
+    "100",
     2,
-    1000,
-    epic.address,
+    "7000000000000000000000000",
+    EPIC_ADDRESS,
     [
-      "0x0ce5ec70ab00dd97c3ab0c269960b576abbcc3640e9d0eb9946da973ad8ddcfa41b38895a81ca2c8d7f0730d0585de723970fcc2a0738a58030e12a658709b431b"
+      signatureOne, signatureTwo
     ]
   );
   await tx.wait(1);
 
-  console.log(tx)
+  // console.log(tx)
 }
 
 module.exports.tags = ['test'];
